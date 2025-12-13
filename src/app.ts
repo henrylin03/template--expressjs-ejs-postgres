4644577b9ba2523dd6d0741531034f13aa8d4c96
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-12-13 14:00:25 +1100

    feat(app.ts): display 'hello world' text on home page route ('/'). refactor(app.ts): use import keyword instead of require
    using import somehow caused types to be not necessary (i.e. i didn't have to explicitly type res as Result, imported from Express anymore)

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
+app.get("/", (_req, res) => res.send("hello world!"));
+
 const PORT = 3000;
-app.listen(PORT, (error: Error) => {
+app.listen(PORT, (error) => {
   if (error) throw Error;
   console.log(`Listening on port ${PORT}`);
 });
