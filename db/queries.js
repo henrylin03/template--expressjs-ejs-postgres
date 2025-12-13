5b02488aca5b4252d8a4c0682796c0c1a815c710
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-12-13 15:32:34 +1100

    chore(queries): add placeholder file for db queries

---
+++ db/queries.js
@@ -0,0 +1,12 @@
+const pool = require("./pool");
+
+/* WRITE QUERIES HERE */
+
+// async function getAllMessages() {
+//   const { rows } = await pool.query("SELECT * FROM messages");
+//   return rows;
+// }
+
+// module.exports = {
+//   getAllMessages,
+// };
