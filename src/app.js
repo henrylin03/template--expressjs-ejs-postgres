d2c6794ad99e65cea6a15c3bf30aa32deb058d35
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-12-13 14:41:27 +1100

    style: install biome, apply biome

--- src/app.js
+++ src/app.js
@@ -5,6 +5,6 @@ app.get("/", (_req, res) => res.send("hello world!"));
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
-  if (error) throw Error;
-  console.log(`Listening on port ${PORT}`);
+	if (error) throw Error;
+	console.log(`Listening on port ${PORT}`);
 });
