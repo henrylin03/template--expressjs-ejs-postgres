430dea8aacd79a60cab5d7006614b5e3cfac1564
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-09 13:47:31 +1100

    chore: npm audit fix vulnerabilities. use type: module.
    type
    : module allows for import keyword instead of require

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const path = require("node:path");
+import path from "node:path";
+import express from "express";
 
-const app = express();
+// const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
-/* set up EJS templating */
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+// /* set up EJS templating */
+// app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
 
-/* middleware to parse data in request body */
-app.use(express.urlencoded({ extended: true }));
+// /* middleware to parse data in request body */
+// app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (_req, res) => res.render("index"));
+// app.get("/", (_req, res) => res.render("index"));
 
-const PORT = 3000;
-app.listen(PORT, (error) => {
-	if (error) throw Error;
-	console.log(`Listening on port ${PORT}`);
-});
+// const PORT = 3000;
+// app.listen(PORT, (error) => {
+//   if (error) throw Error;
+//   console.log(`Listening on port ${PORT}`);
+// });
