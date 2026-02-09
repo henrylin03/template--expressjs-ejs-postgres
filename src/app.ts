c0d454ebf130387e2a901fd1fbb52dcd33c7ed3d
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-09 14:03:56 +1100

    feat(app.ts): display ejs template

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
+import path from "node:path";
 import express from "express";
+
+const app = express();
+
+const currentPath = import.meta.dirname;
+
+app.use(express.static(path.join(currentPath, "..", "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(currentPath, "views"));
+
+app.get("/", (_req, res) => res.render("index"));
+
+const PORT = 3000;
+app.listen(PORT, (err) => {
+	if (err) throw Error;
+	console.log(`Listening on port ${PORT}`);
+});
